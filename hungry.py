abf3c14062114a9dab01daec3ac2a735e9e65d23
surenderlic <74841366+surenderlic@users.noreply.github.com>
2020-12-03 05:39:24 +0530

    Delete hungry.py

--- hungry.py
+++
@@ -1,7 +0,0 @@
-hungry = input("are you hungry?")
-print("welcome to python programming")
-if hungry == "yes":
-    print("eat samosa")
-else:
-    print("do your home work")
-print("thank you for giving your response")
